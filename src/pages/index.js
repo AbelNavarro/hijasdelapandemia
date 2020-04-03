c7f4da0d3fac8e86daf86dea72811064542b0888
Abel Navarro <abel.navarro@webfleet.com>
2020-04-03 11:05:24 +0200

    trimming stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Hijas de la Pandemia" />
     <h1>Hijas de la Pandemia</h1>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
